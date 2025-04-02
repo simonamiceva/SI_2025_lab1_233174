d96592325ac6362a554650499b293eb93ac938ed
Simona Miceva <simona.miceva18@gmail.com>
2025-04-02 12:09:42 +0200

    Implement sortTasksByName function

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -65,17 +65,19 @@ class TaskManager {
     // 1. Remove a task by name
     public void removeTask(String name) {
         // TODO: Implement removal logic
+      tasks.removeIf(task -> task.getName().equalsIgnoreCase(taskName));
     }
 
     // 2. Find all completed tasks
     public List<Task> getCompletedTasks() {
         // TODO: Implement logic to return completed tasks
-        return new ArrayList<>();
+        return tasks.stream().filter(Task::isCompleted).collect(Collectors.toList());
     }
 
     // 3. List tasks sorted by name
     public void sortTasksByName() {
         // TODO: Implement sorting logic
+      tasks.sort(Comparator.comparing(Task::getName));
     }
 
     // 4. Sort tasks by priority
